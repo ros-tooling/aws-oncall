1da9b09627c84a2dabe0f26f77b4e9fb86081b2b
Anas Abou Allaban <allabana@amazon.com>
2020-03-02 09:37:17 -0800

    Fix email workflow (#86)
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>

--- .github/daily-report-sendgrid.js
+++ .github/daily-report-sendgrid.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const htmlReport = fs.readFileSync('./report.html', 'utf8');
+const htmlReport = fs.readFileSync('./daily-report.html', 'utf8');
 
 const msg = {
 to: 'aws-robotics-foundations-oncall@amazon.com',

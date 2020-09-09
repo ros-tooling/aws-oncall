4c60c9b9b60013e69331e1c8665bc3db1da1ad06
Devin Bonnie <dbbonnie@amazon.com>
2020-09-09 10:20:54 -0700

    Update sendgrid email destination
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>

--- .github/daily-report-sendgrid.js
+++ .github/daily-report-sendgrid.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const htmlReport = fs.readFileSync('./daily-report.html', 'utf8');
 
 const msg = {
-to: 'aws-robotics-foundations-oncall@amazon.com',
+to: 'aws-robotics-foundations-ecosystem-oncall@amazon.com',
 from: 'test@example.com',
 subject: '[ACTION NEEDED] Oncall Task: please ping open PRs without activity for a day or more',
 html: htmlReport,
